c3a0039f3d1025004c93e208f62d94816b3ca0c5
Manuel <m.a.c.p90@gmail.com>
2017-11-04 15:01:23 -0400

    class b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BuiltInPipesComponent } from './built-in-pipes/built-in-pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuiltInPipesComponent
   ],
   imports: [
     BrowserModule
